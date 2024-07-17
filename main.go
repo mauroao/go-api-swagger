35e88effffa1214f69d0c7e448dd63701c4a591a
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2024-07-17 20:54:12 -0300

    add hello endpoint

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello %s", "World\n")
+	http.HandleFunc("GET /api/hello", helloHandler)
+
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello!!!")
 }
